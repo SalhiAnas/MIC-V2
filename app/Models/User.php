b82897e1b3d60aae6089584e97f87bb64dfcb6d6
anas- <anas.salhi6@gmail.com>
2021-10-27 13:22:46 +0100

    fixing history issues

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * The attphpributes that should be cast to native types.
      *
      * @var array
      */
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema ;
 
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191) ;
+        User::deleting(function ($user) {
+            $user->history()->delete();
+        });
     }
 }
